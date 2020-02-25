61a9c059efb90971fe9f3e69c36831d59c81b249
vjohnmarshal99 <61184916+vjohnmarshal99@users.noreply.github.com>
2020-02-25 15:18:43 +0530

    Add files via upload

---
+++ arithemetic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f;
+printf("enter the values of a and b");
+scanf("%d%d", &a, &b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("sum of two numbers=",c);
+printf("diff of two no=",d);
+printf("mul of two no=",e);
+printf("div of two no=",f);
+}
