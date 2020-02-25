455e8f893b81f87e59516e368d13b0e6af60ee40
vjohnmarshal99 <61184916+vjohnmarshal99@users.noreply.github.com>
2020-02-25 16:01:26 +0530

    Add files via upload

---
+++ and.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int age;
+printf("enter your age");
+scanf("%d",&age);
+if(age<=18 && age>=13)
+{
+printf("you are a teenager");
+}
+else
+{
+printf("you are not a teenager");
+}
+}
