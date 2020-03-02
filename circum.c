7c2469eb4d0569790a6126563a1a0d015b0ca81a
vjohnmarshal99 <61184916+vjohnmarshal99@users.noreply.github.com>
2020-03-02 08:42:51 +0530

    Add files via upload

---
+++ circum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float area,r,circum;
+printf("enter the value of r");
+scanf("%f",&r);
+area=(3.14*r*r);
+circum=(2*3.14*r);
+printf("area of circle=%f",area);
+printf("circumference of the circle=%f",circum);
+}
