e13ae02295698f8d75971f90a32dae4e98f687fd
vjohnmarshal99 <61184916+vjohnmarshal99@users.noreply.github.com>
2020-03-02 09:44:41 +0530

    Add files via upload

---
+++ salary.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+float g,b,h,d;
+printf("enter the value of b\n");
+scanf("%f",&b);
+h=0.2*b;
+printf("h=%f",h);
+d=0.4*(b);
+printf("d=%F",d);
+g=h+b+d;
+printf("g=%F",g);
+}
+
