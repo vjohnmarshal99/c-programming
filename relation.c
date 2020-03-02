cf37534fb1b42fa04226c0f24f54799890352526
vjohnmarshal99 <61184916+vjohnmarshal99@users.noreply.github.com>
2020-03-02 09:04:31 +0530

    Add files via upload

---
+++ relation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the value of a and b");
+scanf("%d%d",&a,&b);
+printf("%d",a>b);
+printf("%d",a<b);
+printf("%d",a==b);
+printf("%d",a>=b);
+printf("%d",a<=b);
+printf("%d",a!=b);
+}
