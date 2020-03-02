b894ce31f3e4e5b588510d9b932686972fa011a8
vjohnmarshal99 <61184916+vjohnmarshal99@users.noreply.github.com>
2020-03-02 09:12:26 +0530

    Add files via upload

---
+++ totavg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float total,m1,m2,m3,m4,m5,avg;
+printf("enter the values of m1,m2,m3,m4,m5");
+scanf("%f%f%f%f%f",&m1,&m2,&m3,&m4,&m5);
+total=m1+m2+m3+m4+m5;
+avg=(m1+m2+m3+m4+m5)/5;
+printf("total marks=%f",total);
+printf("average marks=%f",avg);
+}
+
