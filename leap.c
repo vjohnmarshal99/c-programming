e5a601b4adf622acefa47eef4d681025e7c452d4
vjohnmarshal99 <61184916+vjohnmarshal99@users.noreply.github.com>
2020-03-09 09:51:50 +0530

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int days;
+printf("enter the value of days");
+scanf("%d",&days);
+if(days/4==0)
+{
+printf("it is a leap year");
+}
+else
+{
+printf("it is not leap year");
+}
+}
